4f0fe6af683e9686ba9368af376f947a9e0f1973
Yaprak <121570152+tYaprakYank@users.noreply.github.com>
2023-05-31 22:57:32 +0300

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using occupy.Models;
+using occupy.Models.Entities;
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace occupy.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ILogger<AdminController> _logger;
+
+    public AdminController(ILogger<AdminController> logger)
+    {
+        _logger = logger;
+    }
+
+    ParamatordbContext db = new ParamatordbContext();
+
+    [Authorize]
+    public IActionResult Index()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.FirstOrDefault(), };
+        return View(model);
+    }
+
+    [Route("/signin")]
+    public IActionResult SignIn()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.FirstOrDefault(), };
+        return View(model);
+    }
+
+    [HttpPost]
+    [IgnoreAntiforgeryToken]
+    [Route("/admin/signin")]
+    public async Task<IActionResult> SignIn(User postedData)
+    {
+        User user = db.Users!.FirstOrDefault(
+            x => x.Username == postedData.Username && x.Password == postedData.Password
+        )!;
+
+        if (user != null)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("user", user.Id.ToString()),
+                new Claim("role", "admin")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+            var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
+
+            await HttpContext.SignInAsync(claimsPrinciple);
+
+            TempData["SuccessSignIn"] = "Giriş yapıldı.";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            TempData["Danger"] = "Hatalı kullanıcı adı ve ya şifre.";
+            return Redirect("/signin");
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using occupy.Models;
+using occupy.Models.Entities;
+
+namespace occupy.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    ParamatordbContext db = new ParamatordbContext();
+
+    public IActionResult Index()
+    {
+        var model = new IndexViewModel()
+        {
+            Services = db.Services!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Abouts = db.Abouts!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Slides = db.Slides!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Site = db.Sites!.FirstOrDefault(),
+            Teams = db.Teams!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Messages = db.Messages!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+        };
+        return View(model);
+    }
+
+    [Route("/about-us")]
+    public IActionResult About()
+    {
+        var model = new IndexViewModel()
+        {
+            Teams = db.Teams!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            About = db.Abouts!.FirstOrDefault(x => x.Isview == true),
+            Abouts = db.Abouts!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Site = db.Sites!.First(),
+            Service = db.Services!.FirstOrDefault(x => x.Isview == true),
+            Services = db.Services!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+            Messages = db.Messages!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+        };
+        return View(model);
+    }
+
+    [Route("/service")]
+    public IActionResult Service()
+    {
+        var model = new IndexViewModel()
+        {
+            Site = db.Sites!.First(),
+            Success = db.Successes!.FirstOrDefault(x => x.Isview == true),
+            Successes = db.Successes!.OrderBy(x => x.Order).Where(x => x.Isview == true).ToList(),
+        };
+        return View(model);
+    }
+
+    [Route("/blog")]
+    public IActionResult Blog()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/blog-single/{title}/{id}")]
+    public IActionResult BlogSingle(string title, int id)
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/cource")]
+    public IActionResult Cource()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/cource-single/{title}/{id}")]
+    public IActionResult CourceSingle(string title, int id)
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/event")]
+    public IActionResult Event()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/event-single/{title}/{id}")]
+    public IActionResult EventSingle(string title, int id)
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [Route("/contact")]
+    public IActionResult Contact()
+    {
+        var model = new IndexViewModel() { Site = db.Sites!.First() };
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}
